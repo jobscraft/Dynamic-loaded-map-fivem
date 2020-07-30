b34c4d9289cd087a0199163f7c315b5f8964bc92
jobscraft <mlahouille@outlook.fr>
2020-07-30 20:14:14 +0200

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+description 'Dynamic Loading Map '
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+
+
+client_script 'ScriptTest/testcl.lua'
+server_script 'ScriptTest/testsv.lua'  --this is preview script, you can delete this lines when you want (folder "ScriptTest" too)
+
+
+
+this_is_a_map 'yes'
+fx_version 'adamant'
+games {'gta5'}
